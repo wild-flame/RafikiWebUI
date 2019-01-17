6ab4a4ba5eff17b5631a6a4adf729c5ef9a94f70
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2019-01-17 18:50:53 +0800

    set up redux middlesare boilerplate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,55 @@
+// React and React-DOM
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Redux and Middleware
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+
+// Material-UI
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 import App from './App';
+import rootReducer from "./store/rootReducer"
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// compose to combine store enhancers
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+
+    )
+  )
+);
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // deepblue
+      main: "#1a237e"
+    },
+    secondary: {
+      // red
+      main: "#c62828"
+    }
+  },
+  typography: {
+    htmlFontSize: 14,
+    // Migration to typography v2
+    useNextVariants: true
+  }
+});
+
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
