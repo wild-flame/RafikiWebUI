932f7bac87a472bfdd210124f96213f0d9caabbf
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2019-01-17 23:15:13 +0800

    add react router for App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route 
+ } from 'react-router-dom'
+
 import BoilerPlate from './containers/BoilerPlate/BoilerPlate';
+import LandingPage from "./containers/LandingPage/LandingPage"
+
+
+const NoMatch = ({ location }) => (
+  <h3>No page found for <code>{location.pathname}</code></h3>
+)
 
 
 class App extends Component {
   render() {
     return (
-      <BoilerPlate />
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path='/'
+            component={LandingPage}
+          />
+          <Route
+            exact
+            path='/boilerplate'
+            component={BoilerPlate}
+          />
+          <Route component={NoMatch} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/containers/LandingPage/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+  render() {
+    return "i am a landing page!"
+  }
+}
+
+export default LandingPage
