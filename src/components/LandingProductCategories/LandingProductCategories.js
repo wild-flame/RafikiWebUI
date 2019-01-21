b6c53d05c0a7c0b233c76fd46e2dfbe43b45cd9f
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2019-01-21 17:48:57 +0800

    add categories of applications

--- src/components/LandingProductCategories/LandingProductCategories.js
+++ src/components/LandingProductCategories/LandingProductCategories.js
@@ -4,6 +4,8 @@ import { withStyles } from '@material-ui/core/styles';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import LayoutBody from '../LandingComponents/LayoutBody';
 import Typography from '../LandingComponents/Typography';
+import BlockchainImg from "../../assets/Blockchain_art_July_2018_2_1.jpg"
+import WikiImg from "../../assets/wikipedia-grand-1080x675.jpg"
 
 const styles = theme => ({
   root: {
@@ -88,65 +90,41 @@ function ProductCategories(props) {
 
   const images = [
     {
-      url:
-        'https://images.unsplash.com/photo-1534081333815-ae5019106622?auto=format&fit=crop&w=400&q=80',
-      title: 'Snorkeling',
+      url: BlockchainImg,
+        //'https://www.joc.com/sites/default/files/field_feature_image/Blockchain%20art%20July%202018%202_1.jpg',
+      title: 'Blockchain',
       width: '40%',
     },
     {
-      url:
-        'https://images.unsplash.com/photo-1531299204812-e6d44d9a185c?auto=format&fit=crop&w=400&q=80',
-      title: 'Massage',
-      width: '20%',
+      url: WikiImg,
+        //'http://new.cellconstraintcancer.com/wp-content/uploads/2017/11/wikipedia-grand-1080x675.jpg',
+      title: 'Wiki Engine',
+      width: '60%',
     },
     {
       url:
-        'https://images.unsplash.com/photo-1476480862126-209bfaa8edc8?auto=format&fit=crop&w=400&q=80',
-      title: 'Hiking',
-      width: '40%',
-    },
-    {
-      url:
-        'https://images.unsplash.com/photo-1453747063559-36695c8771bd?auto=format&fit=crop&w=400&q=80',
-      title: 'Tour',
+        'https://c1.staticflickr.com/8/7100/7046439385_b83413587a_b.jpg',
+      title: 'Collaborative Analytics',
       width: '38%',
     },
     {
       url:
-        'https://images.unsplash.com/photo-1523309996740-d5315f9cc28b?auto=format&fit=crop&w=400&q=80',
-      title: 'Gastronomy',
+        'https://www.gov.sg/~/media/gov/factually/thumbnail/health/dnu%20hospital%20mockup.jpg',
+      title: 'Healthcare',
       width: '38%',
     },
     {
       url:
-        'https://images.unsplash.com/photo-1534452203293-494d7ddbf7e0?auto=format&fit=crop&w=400&q=80',
-      title: 'Shopping',
+        'https://upload.wikimedia.org/wikipedia/en/f/f8/NUS_School_of_Computing.JPG',
+      title: 'More',
       width: '24%',
-    },
-    {
-      url:
-        'https://images.unsplash.com/photo-1506941433945-99a2aa4bd50a?auto=format&fit=crop&w=400&q=80',
-      title: 'Walking',
-      width: '40%',
-    },
-    {
-      url:
-        'https://images.unsplash.com/photo-1533727937480-da3a97967e95?auto=format&fit=crop&w=400&q=80',
-      title: 'Fitness',
-      width: '20%',
-    },
-    {
-      url:
-        'https://images.unsplash.com/photo-1518136247453-74e7b5265980?auto=format&fit=crop&w=400&q=80',
-      title: 'Reading',
-      width: '40%',
-    },
+    }
   ];
 
   return (
     <LayoutBody className={classes.root} component="section" width="large">
       <Typography variant="h4" marked="center" align="center" component="h2">
-        For all tastes and all desires
+        For all kinds of applications
       </Typography>
       <div className={classes.images}>
         {images.map(image => (
