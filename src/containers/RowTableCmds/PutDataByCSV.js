4077e129764f6686d018b2a34c04e4f21197a68b
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2019-02-03 22:40:00 +0800

    PutDataByCSV upload success!

--- src/containers/RowTableCmds/PutDataByCSV.js
+++ src/containers/RowTableCmds/PutDataByCSV.js
@@ -13,6 +13,7 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from '@material-ui/core/Switch';
 import Checkbox from "@material-ui/core/Checkbox";
 import Header from '../../components/ConsoleHeader/Header';
 import Dropzone from 'react-dropzone'
@@ -51,25 +52,28 @@ const styles = theme => ({
   }
 })
 
-const currencies = [
+const datasetBranches = [
+  // ls-ds-branch -t ds9
+  // [SUCCESS: LIST_DATASET_BRANCH] Branches: ["master"]
   {
-    value: 'USD',
-    label: '$',
+    dataset: 'ds1',
+    branches: ["master"]
   },
   {
-    value: 'EUR',
-    label: '€',
+    dataset: 'ds9',
+    branches: ["master"]
   },
   {
-    value: 'BTC',
-    label: '฿',
+    dataset: 'BTC',
+    branches: ["master", "dev"]
   },
   {
-    value: 'JPY',
-    label: '¥',
+    dataset: 'JPY',
+    branches: ["master", "newFeature"]
   },
 ];
 
+// for file dropzone
 const baseStyle = {
   width: 300,
   height: 100,
@@ -96,9 +100,25 @@ class PutDataByCSV extends React.Component {
     ResultLoading: false,
     apiRes: "",
     mobileOpen: false,
-    DataSetName: "",
-    BranchName: "",
+    checkedNewDataset: false,
+    checkedNewBranch: false,
+    dataset:"",
+    newDataset:"",
+    branch:"",
+    newBranch:"",
     files: []
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  handleSwitch = name => event => {
+    this.setState({
+      [name]: event.target.checked
+    });
   }
 
   onDrop = (files) => {
@@ -137,6 +157,35 @@ class PutDataByCSV extends React.Component {
     }
   }
 
+  async uploadData() {
+    await this.setState({
+      ResultLoading: true,
+    });
+    // send a POST request
+    // Initial FormData
+    const formData = new FormData();
+    formData.append("file", this.state.files[0]);
+    try {
+      const result = await axios({
+        method: 'post',
+        url: `${HTTPconfig.gateway}api/put-de-by-csv`,
+        headers: {'Content-Type':'multipart/form-data'},//HTTPconfig.HTTP_HEADER,
+        data: formData
+      });
+      await this.setState({
+        ResultLoading: false,
+        apiRes: result.data.result,
+      });
+    } catch (error) {
+      // if server service error
+      console.error(error);
+      // change state for UI
+      await this.setState({
+        ResultLoading: false,
+      });
+    }
+  }
+
   render() {
     const files = this.state.files.map(file => (
       <li key={file.name}>
@@ -144,8 +193,8 @@ class PutDataByCSV extends React.Component {
       </li>
     ))
     const { classes } = this.props;
-    console.log(this.state.apiRes)
-    console.log(this.state.files)
+    console.log("apiRes returned is: ", this.state.apiRes)
+    console.log("state.files is: ", this.state.files[0])
     return (
       <React.Fragment>
         <Header
@@ -162,184 +211,212 @@ class PutDataByCSV extends React.Component {
               </Toolbar>
             </AppBar>
             <div className={classes.contentWrapper}>
-              <Typography variant="h5" gutterBottom align="center">
-                1. Dataset Name
-              </Typography>
-              <Grid
-                container
-                direction="row"
-                justify="space-evenly"
-                alignItems="center"
-              >
-                <Grid item>
-                  <TextField
-                    id="existing-dataset-names"
-                    select
-                    label="Select from datasets"
-                    className={classes.textField}
-                    value={this.state.DataSetName}
-                    onChange={() => {}}
-                    SelectProps={{
-                      MenuProps: {
-                        className: classes.menu,
-                      },
-                    }}
-                    helperText="Please select your dataset"
-                    margin="normal"
-                  >
-                    {currencies.map(option => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </TextField>
-                </Grid>                
-                <Grid item>
-                  <FormControlLabel
-                    className={classes.cock}
-                    control={
-                      <Checkbox
-                        checked={false}
-                        onChange={() => {}}
-                        value="useTeams"
-                      />
-                    }
-                    disabled={false}
-                    label="Create new dataset"
-                  />
-                </Grid>
-                <Grid item>
-                  <TextField
-                    id="new-dataset-name"
-                    label="New Dataset"
-                    className={classes.textField}
-                    value={this.state.DataSetName}
-                    onChange={() => console.log("textfield clicked")}
-                    margin="normal"
-                  />              
-                </Grid>
-              </Grid>
-              <Typography variant="h5" gutterBottom align="center">
-                2. Branch Name
-              </Typography>
-              <Grid
-                container
-                direction="row"
-                justify="space-evenly"
-                alignItems="center"
-              >
-                <Grid item>
-                  <TextField
-                    id="existing-branch-names"
-                    select
-                    label="Default is master"
-                    className={classes.textField}
-                    value={this.state.BranchName}
-                    onChange={() => {}}
-                    SelectProps={{
-                      MenuProps: {
-                        className: classes.menu,
-                      },
-                    }}
-                    helperText="Please select your branch"
-                    margin="normal"
-                  >
-                    {currencies.map(option => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </TextField>
-                </Grid>
-                <Grid item>
-                  <FormControlLabel
-                    className={classes.cock}
-                    control={
-                      <Checkbox
-                        checked={false}
-                        onChange={() => {}}
-                        value="useTeams"
-                      />
-                    }
-                    disabled={false}
-                    label="Create new branch"
-                  />
-                </Grid>
-                <Grid item>
-                  <TextField
-                    id="new-branch-name"
-                    label="New Branch"
-                    className={classes.textField}
-                    value={this.state.BranchName}
-                    onChange={() => console.log("textfield clicked")}
-                    margin="normal"
-                  />              
-                </Grid>
-              </Grid>
-              <Typography variant="h5" gutterBottom align="center">
-                3. Upload CSV
-              </Typography>
-              <Dropzone
-                accept={"text/plain, application/vnd.ms-excel"}
-                onDrop={this.onDrop}
-                multiple={false}
-              >
-                {({ getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject }) => {
-                  let styles = {...baseStyle}
-                  styles = isDragActive ? {...styles, ...activeStyle} : styles
-                  styles = isDragReject ? {...styles, ...rejectStyle} : styles
-
-                  return (
-                    <div
-                      {...getRootProps()}
-                      style={styles}
+              <Grid container spacing={24}>
+                <Grid item xs={6}>
+                  <Paper>
+                    <Typography variant="h5" gutterBottom align="center">
+                      1. Dataset Name
+                    </Typography>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="space-evenly"
+                      alignItems="center"
                     >
-                      <input {...getInputProps()} />
-                      <div>
-                        {isDragAccept ? 'Drop' : 'Drag'} files here...
-                      </div>
-                      {isDragReject && <div>Unsupported file type...</div>}
-                    </div>
-                  )
-                }}
-              </Dropzone>
-              <h4>CSV File:</h4>
-              <ul>{files}</ul>
-              <br />
-              <Typography variant="h5" gutterBottom align="center">
-                4. Dataset Schema
-              </Typography>
-              <Grid
-                container
-                direction="row"
-                justify="space-evenly"
-                alignItems="center"
-              >
-                <Grid item>
-                  <FormControlLabel
-                    className={classes.cock}
-                    control={
-                      <Checkbox
-                        checked={false}
-                        onChange={() => {}}
-                        value="useTeams"
-                      />
-                    }
-                    disabled={false}
-                    label="CSV First Row as Schema"
-                  />
+                      <Grid item>
+                        <TextField
+                          id="existing-dataset-names"
+                          select
+                          label="Select from datasets"
+                          className={classes.textField}
+                          value={!this.state.checkedNewDataset && this.state.dataset}
+                          onChange={this.handleChange("dataset")}
+                          SelectProps={{
+                            MenuProps: {
+                              className: classes.menu,
+                            },
+                          }}
+                          helperText="Please select your dataset"
+                          margin="normal"
+                          disabled={this.state.checkedNewDataset}
+                        >
+                          {datasetBranches.map(option => (
+                            <MenuItem key={option.dataset} value={option.dataset}>
+                              {option.dataset}
+                            </MenuItem>
+                          ))}
+                        </TextField>
+                      </Grid>                
+                      <Grid item>
+                        <FormControlLabel
+                          control={
+                            <Switch
+                              checked={this.state.checkedNewDataset}
+                              onChange={this.handleSwitch("checkedNewDataset")}
+                              value="checkedNewDataset"
+                            />
+                          }
+                          label="Create new dataset"
+                        />
+                      </Grid>
+                      <Grid item>
+                        <TextField
+                          id="new-dataset-name"
+                          label="New Dataset"
+                          className={classes.textField}
+                          value={this.state.checkedNewDataset && this.state.newDataset}
+                          onChange={this.handleChange("newDataset")}
+                          margin="normal"
+                          disabled={!this.state.checkedNewDataset}
+                        />              
+                      </Grid>
+                    </Grid>
+                    <Typography variant="h5" gutterBottom align="center">
+                      2. Branch Name
+                    </Typography>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="space-evenly"
+                      alignItems="center"
+                    >
+                      <Grid item>
+                        <TextField
+                          id="existing-branch-names"
+                          select
+                          label="Default is master"
+                          className={classes.textField}
+                          value={!this.state.checkedNewBranch && this.state.branch}
+                          onChange={this.handleChange("branch")}
+                          SelectProps={{
+                            MenuProps: {
+                              className: classes.menu,
+                            },
+                          }}
+                          helperText="Please select your branch"
+                          margin="normal"
+                          disabled={this.state.checkedNewBranch}
+                        >
+                          {this.state.dataset
+                            ? (datasetBranches.filter(item => item.dataset === this.state.dataset)[0]
+                                .branches.map(item => (
+                                  <MenuItem key={item} value={item}>
+                                    {item}
+                                  </MenuItem>
+                                ))
+                            )
+                            : (
+                              <MenuItem value={"master"}>
+                                {"master"}
+                              </MenuItem>
+                            )
+                          }
+                        </TextField>
+                      </Grid>
+                      <Grid item>
+                        <FormControlLabel
+                          control={
+                            <Switch
+                              checked={this.state.checkedNewBranch}
+                              onChange={this.handleSwitch("checkedNewBranch")}
+                              value="checkedNewBranch"
+                            />
+                          }
+                          label="Create new branch"
+                        />
+                      </Grid>
+                      <Grid item>
+                        <TextField
+                          id="new-branch-name"
+                          label="New Branch"
+                          className={classes.textField}
+                          value={this.state.checkedNewBranch && this.state.newBranch}
+                          onChange={this.handleChange("newBranch")}
+                          margin="normal"
+                          disabled={!this.state.checkedNewBranch}
+                        />              
+                      </Grid>
+                    </Grid>
+                    <Typography variant="h5" gutterBottom align="center">
+                      3. Upload CSV
+                    </Typography>
+                    <Dropzone
+                      accept={"*.csv, text/csv, application/vnd.ms-excel"}
+                      onDrop={this.onDrop}
+                      multiple={false}
+                    >
+                      {({ getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject }) => {
+                        let styles = {...baseStyle}
+                        styles = isDragActive ? {...styles, ...activeStyle} : styles
+                        styles = isDragReject ? {...styles, ...rejectStyle} : styles
+
+                        return (
+                          <div
+                            {...getRootProps()}
+                            style={styles}
+                          >
+                            <input {...getInputProps()} />
+                            <div>
+                              {isDragAccept ? 'Drop' : 'Drag'} files here...
+                            </div>
+                            {isDragReject && <div>Unsupported file type...</div>}
+                          </div>
+                        )
+                      }}
+                    </Dropzone>
+                    <h4>CSV File:</h4>
+                    <ul>{files}</ul>
+                    <br />
+                    <Typography variant="h5" gutterBottom align="center">
+                      4. Dataset Schema
+                    </Typography>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="space-evenly"
+                      alignItems="center"
+                    >
+                      <Grid item>
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={this.state.checkedNewDataset}
+                              value="checkedWithSchema"
+                            />
+                          }
+                          disabled={false}
+                          label="CSV First Row as Schema"
+                        />
+                      </Grid>
+                    </Grid>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="flex-end"
+                      alignItems="center"
+                    >
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => this.uploadData()}
+                    >
+                      COMMIT
+                    </Button>
+                    </Grid>
+                  </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                  <Paper>
+                    <Typography variant="h5" gutterBottom align="center">
+                      Forkbase Status:
+                    </Typography>
+                    <div>...</div>
+                    <div>...</div>
+                    <div>...</div>
+                  </Paper>
                 </Grid>
               </Grid>
-              <Grid
-                container
-                direction="row"
-                justify="flex-end"
-                alignItems="center"
-              >
-              <Button variant="contained" color="primary">
-                COMMIT
-              </Button>
-              </Grid>
+ 
             </div>
           </Paper>
         </main>
