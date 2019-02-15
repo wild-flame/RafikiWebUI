2f974c7682c4e04a09169a9aa28120e58c04f778
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2019-02-15 11:16:47 +0800

    ForkbaseStatus component

---
+++ src/components/ConsoleContents/ForkbaseStatus.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = () => ({
+  subPaper: {
+    minHeight: 150
+  },
+  forkbaseStatus: {
+    padding: 20
+  }
+})
+
+class ForkbaseStatus extends React.Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    children: PropTypes.node,
+  }
+
+  render() {
+    const { classes, children } = this.props;
+    return (
+      <Paper className={classes.subPaper}>
+        <Typography variant="h5" gutterBottom align="center">
+          Forkbase Status:
+        </Typography>
+        <div className={classes.forkbaseStatus}>
+          {children}
+        </div>
+      </Paper>
+    )
+  }
+}
+
+export default withStyles(styles)(ForkbaseStatus)
+
--- src/containers/RowTableCmds/PutDataByCSV.js
+++ src/containers/RowTableCmds/PutDataByCSV.js
@@ -9,7 +9,6 @@ import * as actions from "./actions"
 import { withStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import FormControlLabel from "@material-ui/core/FormControlLabel";
@@ -22,6 +21,7 @@ import ContentBar from "../../components/ConsoleContents/ContentBar"
 import CsvDropzone from "../../components/ConsoleContents/CsvDropzone"
 import DatasetName from "../../components/ConsoleContents/DatasetName"
 import BranchName from "../../components/ConsoleContents/BranchName"
+import ForkbaseStatus from "../../components/ConsoleContents/ForkbaseStatus"
 
 // RegExp rules
 import { validDsAndBranch } from "../../regexp-rules";
@@ -394,10 +394,7 @@ class PutDataByCSV extends React.Component {
                 </Grid>
               </Grid>
               <Grid item xs={6}>
-                <Paper>
-                  <Typography variant="h5" gutterBottom align="center">
-                    Forkbase Status:
-                  </Typography>
+                <ForkbaseStatus>
                   <Typography component="p">
                     <b>{Response_CreateDS[0]}</b>
                     <br />
@@ -419,7 +416,7 @@ class PutDataByCSV extends React.Component {
                     <br />
                     {Response_PutDataCSV[2]}
                   </Typography>
-                </Paper>
+                </ForkbaseStatus>
               </Grid>
             </Grid>
           </div>
