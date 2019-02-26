99f4139093647a1cce701cc0a95c85f3951eb8e4
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2019-02-26 12:16:34 +0800

    fix storagebar regex character class

--- src/components/ConsoleContents/StorageBar.js
+++ src/components/ConsoleContents/StorageBar.js
@@ -24,7 +24,7 @@ class StorageBar extends React.Component {
     // parse the StorgaeSize string (eg 1.45MB, or 763.9KB) into number and unit
     // const NumPart = Math.floor(parseFloat(StorageSize) / 10.24)
     const NumPart = parseFloat(StorageSize)
-    const UnitPart = StorageSize.replace(/[0-9]./g, '')
+    const UnitPart = StorageSize.replace(/[0-9.]/g, '')
     console.log(NumPart, UnitPart) // MB KB
 
     let PercentNum = 1
