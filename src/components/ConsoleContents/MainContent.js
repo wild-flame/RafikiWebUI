55be79fe451d122460f03647f30c48deaecc9458
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2019-02-27 10:31:50 +0800

    add Fab for all RowTable routes

--- src/components/ConsoleContents/MainContent.js
+++ src/components/ConsoleContents/MainContent.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { compose } from "redux";
+import { Link, withRouter } from "react-router-dom";
 
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 
+// zoom addicon
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+import ListDSIcon from '@material-ui/icons/FormatListBulleted'
 
-const styles = () => ({
+
+const styles = theme => ({
   mainContent: {
     flex: 1,
     padding: '48px 36px 0',
@@ -15,26 +22,54 @@ const styles = () => ({
     maxWidth: 936,
     margin: 'auto',
     overflow: 'hidden',
-    marginBottom: 20
-  }
+    marginBottom: 20,
+    position: "relative",
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing.unit * 3,
+    right: theme.spacing.unit * 3,
+    zIndex: 10
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
 })
 
 class MainContent extends React.Component {
   static propTypes = {
     classes: PropTypes.object.isRequired,
     children: PropTypes.node,
+    location: PropTypes.object,
   }
 
   render() {
-    const { classes, children } = this.props;
+    const { classes, children, location } = this.props;
     return (
       <main className={classes.mainContent}>
         <Paper className={classes.paper}>
           {children}
+          {location.pathname !== "/console/row-based-table/list-dataset" && (
+            <Zoom in={true} unmountOnExit>
+              <Fab
+                variant="extended"
+                className={classes.fab}
+                color="primary"
+                component={Link}
+                to="/console/row-based-table/list-dataset"
+              >
+                <ListDSIcon className={classes.extendedIcon} />
+                List Dataset
+              </Fab>
+            </Zoom>
+          )}
         </Paper>
       </main>
     )
   }
 }
 
-export default withStyles(styles)(MainContent)
+export default compose(
+  withRouter,
+  withStyles(styles)
+)(MainContent)
